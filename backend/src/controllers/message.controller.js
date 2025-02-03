19c548c0cfbb644c255dbae689842413d16c3bb0
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-02-03 20:51:58 +0600

    get message for a particular user as an authenticated user

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,3 +1,4 @@
+import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
 export const getUsersForSidebar = async (req, res) => {
@@ -7,7 +8,26 @@ export const getUsersForSidebar = async (req, res) => {
 
         res.status(200).json(filteredUsers);
     } catch (error) {
-        console.error("Error in getUsersForSidebar", error);
+        console.error("Error in getUsersForSidebar controller", error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+};
+
+export const getMessages = async (req, res) => {
+    try {
+        const { id:userToChatId } = req.params;
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+            $or: [
+                { sender: myId, receiver: userToChatId },
+                { sender: userToChatId, receiver: myId }
+            ]
+        });
+
+        res.status(200).json(messages);
+    } catch (error) {
+        console.error("Error in getMessages controller", error);
         res.status(500).json({ message: "Internal Server Error" });
     }
 };
