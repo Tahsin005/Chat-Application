4a0cba3d29d0f4b0e5d3e6f11e65bcfd1e8044c1
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-02-03 20:07:33 +0600

    implemented api/auth/update-profile/ route

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcrypt';
 import { generateToken } from '../lib/utils.js';
+import cloudinary from '../lib/cloudinary.js';
 
 export const signup = async (req, res) => {
     const {fullName, email, password} = req.body;
@@ -77,7 +78,26 @@ export const logout = (req, res) => {
         })
         res.status(200).json({ message: 'Logged out successfully' });
     } catch (error) {
-        console.log('Error in login controller', error.message);
+        console.log('Error in logout controller', error.message);
+        res.status(500).json({ message: 'Internal Server Error' });
+    }
+};
+
+export const updateProfile = async (req, res) => {
+    try {
+        const { profilePic } = req.body;
+        const userId = req.user._id;
+
+        if (!profilePic) {
+            return res.status(400).json({ message: 'Please provide a profile picture' });
+        }
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+        const updatedUser = await User.findByIdAndUpdate(userId, { profilePic: uploadResponse.secure_url }, { new: true });
+
+        res.status.json(updatedUser);
+    } catch (error) {
+        console.log('Error in update profile controller', error.message);
         res.status(500).json({ message: 'Internal Server Error' });
     }
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from 'cookie-parser';
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
@@ -7,8 +8,10 @@ import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 const PORT = process.env.PORT;
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
